28cec4c52c414f86256a64062c61634e625f6e3b
saya-nel <pablobellotw@gmail.com>
2020-03-20 15:18:24 +0100

    Squelette pour recherche dans la pile.

--- minizam-full/src/mlvalues.h
+++ minizam-full/src/mlvalues.h
@@ -5,18 +5,29 @@
 
 typedef int64_t mlvalue;
 typedef uint64_t header_t;
-typedef enum { WHITE, GRAY, BLACK } color_t;
-typedef enum { ENV_T, CLOSURE_T, BLOCK_T } tag_t;
+typedef enum
+{
+  WHITE,
+  GRAY,
+  BLACK
+} color_t;
+typedef enum
+{
+  ENV_T,
+  CLOSURE_T,
+  BLOCK_T,
+  FWD_PTR_T
+} tag_t;
 
 /* If a mlvalue ends with 1, it's an integer, otherwise it's a pointer. */
-#define Is_long(v)  (((v) & 1) != 0)
-#define Is_block(v) (((v) & 1) == 0)
+#define Is_long(v) (((v)&1) != 0)
+#define Is_block(v) (((v)&1) == 0)
 
 #define Val_long(v) (((v) << 1) + 1)
 #define Long_val(v) (((uint64_t)(v)) >> 1)
 
 #define Val_ptr(p) ((mlvalue)(p))
-#define Ptr_val(v) ((mlvalue*)(v))
+#define Ptr_val(v) ((mlvalue *)(v))
 #define Val_hd(hd) ((mlvalue)(hd))
 
 /* Structure of the header:
@@ -25,47 +36,44 @@ typedef enum { ENV_T, CLOSURE_T, BLOCK_T } tag_t;
      +--------+-------+-----+
 bits  63    10 9     8 7   0
 */
-#define Size_hd(hd)  ((hd) >> 10)
+#define Size_hd(hd) ((hd) >> 10)
 #define Color_hd(hd) (((hd) >> 8) & 3)
-#define Tag_hd(hd)   ((hd) & 0xFF)
+#define Tag_hd(hd) ((hd)&0xFF)
 
-#define Hd_val(v) (((header_t*)(v))[-1])
-#define Field(v,n) (Ptr_val(v)[n])
-#define Field0(v) Field(v,0)
-#define Field1(v) Field(v,1)
+#define Hd_val(v) (((header_t *)(v))[-1])
+#define Field(v, n) (Ptr_val(v)[n])
+#define Field0(v) Field(v, 0)
+#define Field1(v) Field(v, 1)
 #define Size(v) Size_hd(Hd_val(v))
 #define Color(v) Color_hd(Hd_val(v))
-#define Tag(v)  Tag_hd(Hd_val(v))
+#define Tag(v) Tag_hd(Hd_val(v))
 
 #define WHITE 0
 #define GRAY 1
 #define BLACK 2
-#define Make_header(size,color,tag)                                     \
-  ((header_t)(((size) << 10) | (((color) & 3) << 8) | ((tag) & 0xFF)))
+#define Make_header(size, color, tag) \
+  ((header_t)(((size) << 10) | (((color)&3) << 8) | ((tag)&0xFF)))
 
 #define Addr_closure(c) Long_val(Field0(c))
-#define Env_closure(c)  Field1(c)
+#define Env_closure(c) Field1(c)
 
 mlvalue make_empty_block(tag_t tag);
 mlvalue make_block(size_t size, tag_t tag);
 
 #define Make_empty_env() make_empty_block(ENV_T)
-#define Make_env(size) make_block(size,ENV_T)
+#define Make_env(size) make_block(size, ENV_T)
 
 mlvalue make_closure(uint64_t addr, mlvalue env);
 
 #define Unit Val_long(0)
 
-
 void print_val(mlvalue val);
-char* val_to_str(mlvalue val);
-
+char *val_to_str(mlvalue val);
 
 /* A bytecode is represented as a uint64_t. */
 typedef uint64_t code_t;
 
-int print_instr(code_t* prog, int pc);
-void print_prog(code_t* code);
-
+int print_instr(code_t *prog, int pc);
+void print_prog(code_t *code);
 
 #endif /* _MLVALUES_H */
--- minizam-full/src/stop_and_copy.c
+++ minizam-full/src/stop_and_copy.c
@@ -1,10 +1,5 @@
 #include "stop_and_copy.h"
 
-enum Tag_enum
-{
-  FWD_PTR_T
-};
-
 /*
   Nettoie le tas passer en paramètre
 */
@@ -25,15 +20,38 @@ int heap_can_alloc(size_t n)
 void search_alive_values_on_stack()
 {
 
-  mlvalue *curr = Caml_state->from_space + 1; // +1 pour skipper le premier header
-  while (curr < Caml_state->heap_pointer)
+  // mlvalue *curr = Caml_state->from_space + 1; // +1 pour skipper le premier header
+  // while (curr < Caml_state->heap_pointer)
+  // {
+  //   // print_val(Val_ptr(curr));
+  //   // printf("\n");
+  //   printf("tag : %lu | ", Tag(Val_ptr(curr)));
+  //   curr += Size(Val_ptr(curr)) + 1; // +1 pour skipper le header du bloc suivant
+  // }
+  // printf("\n");
+
+  mlvalue *curr = Caml_state->stack; // premier element de la pile
+  mlvalue *stack_end = Caml_state->stack + (Stack_size / sizeof(mlvalue));
+  while (curr < stack_end) // on parcours toute la pile
   {
-    // print_val(Val_ptr(curr));
-    // printf("\n");
-    printf("tag : %lu | ", Tag(Val_ptr(curr)));
-    curr += Size(Val_ptr(curr)) + 1; // +1 pour skipper le header du bloc suivant
+    if (Is_block(Val_ptr(curr))) // si on est sur un pointeur
+    {
+      // on regarde si le tag est FWD_PTR_T
+      if (Tag(Val_ptr(curr)) == FWD_PTR_T)
+      {
+        // on pointe vers la valeur du forwarding pointer
+      }
+      else
+      {
+        // on créer une copie dans to_space
+
+        // on change son tag dans from_space
+
+        // on pointe vers le nouvel objet dans to_space
+      }
+    }
+    curr += 1; // on va sur l'élément suivant dans la pile
   }
-  printf("\n");
 }
 
 mlvalue *stop_and_copy_alloc(size_t n)
