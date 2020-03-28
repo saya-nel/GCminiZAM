49b27a7a62cb19e7fbb6159095200e08bde78ce9
Loïc Sylvestre <loic.sylvestre@outlook.fr>
2020-03-28 01:51:30 +0100

    mark&sweep : macros pour manipuler des listes

--- minizam-full/src/mark_and_sweep.c
+++ minizam-full/src/mark_and_sweep.c
@@ -4,15 +4,15 @@
 
 #include "domain_state.h"
 #include "config.h"
-
 #include "interp.h"
 
-#include "freelist.h"
+#define Debug(x)// (x)
 
+#define Set_color_hd(hd,color) hd = Make_header(Size_hd(hd),color,Tag_hd(hd));
 
-#define Debug(x) //(x)
+#define Set_color(v,color) Hd_val(v) = Make_header(Size(v),color,Tag(v));
 
-// listes d'objets alloués (pour amorcer les freelist)
+// listes d'objets alloués
 struct cell {
   mlvalue * content;
   struct cell * next;    
@@ -21,94 +21,114 @@ typedef struct cell * list;
 
 #define Empty (0)
 
-void cons (mlvalue * x, list * l){
-  list c = malloc(sizeof (list));
-  assert (c);
-  c->content = x;
-  c->next = *l;
-  *l = c;
-}
+/* list c */
+#define Free_cell(c) do {          \
+  free(c->content);                \
+  free(c);                         \
+  } while (0);
+
+/* (mlvalue * x, list * l) */
+#define Cons(x,l) do {             \
+  list c = malloc(sizeof(list));   \
+  assert (c);                      \
+  c->content = (mlvalue *) x;      \
+  c->next = * ((list *)l);         \
+  *l = c;                          \
+  } while (0);                     
+
+#define Cdr(l) do {                  \
+  list c = *l;                       \
+  *l = (*l)->next;                   \
+  Free_cell(c);                      \
+  } while (0);
+
+#define RemoveCadr(l) do {           \
+  list c = (*l)->next;               \
+  (*l)->next = (*l)->next->next;     \
+  Free_cell(c);                      \
+  } while (0);
+
 
 int length (list l){
   int n = 0;
-  while (l != Empty){
-    ++n;
-    l = l->next; 
-  }
+  while (l != Empty){ ++n; l = l->next; }
   return n;
 }
 
 //liste de gros objets alloués
 static list big_objects = Empty;
 
-// freelist de gros objets
-static mlvalue fl_big_objects = NilFL;
-
 // allouer un gros objet
 mlvalue * alloc_big_object(size_t sz){
-  mlvalue b,*p = NilFL;
-  b = first_fit(sz,&fl_big_objects); 
-  if (b == NilFL){
-    p = malloc(8*sz);
-    cons (p,&big_objects);
-    return p;
-  }
-  return Ptr_val(b);
+  mlvalue *p = malloc(sz*8);
+  //printf ("---->%d\n",(length (big_objects)));
+  Cons(p,&big_objects);
+  return p;
 }
 
 static void mark_aux(mlvalue racine){
   size_t i;
-  if (Is_block(racine)){   
-    if (Color(racine) == WHITE){ // si pas vu
-      Hd_val(racine) = Make_header(Size(racine),GRAY,Tag(racine)); // color[racine] <- vu
+  if (Is_block(racine)){ 
+    //printf("sz = %lld, color = %d, tag = %d\n",Size(racine),Color(racine),Tag(racine));  
+    if (Color(racine) != BLACK){ // si pas vu
+      Set_color(racine,BLACK); // color[racine] <- vu
       for (i = 0; i < Size(racine);i++){
-	mark_aux (Field(racine,i));          
+        mark_aux (Field(racine,i));
       }
     }
   }
 }
+
 void mark(){ 
-  // racines dans la pile
-  // TODO : racine dans les registres
   size_t i;
-  for (i = 0; i < sp; i++){
-    mark_aux(Caml_state->stack[i]);
+  for (i = 0; i < sp; i++){ 
+    mark_aux(Caml_state->stack[i]); 
   }
+  mark_aux(accu);
+  mark_aux(env);
 }
 
 // pour gros objets
-void sweep(list * lst){
-  list cur = *lst;
+void sweep_free(list * lst){
+  list cur;
   mlvalue * b;
-  if (cur != Empty){
-    for ( ;cur->next != Empty ; cur = cur->next){
+  if (*lst != Empty){
+    b = (*lst)->content;
+    if (Color_hd(*b) == WHITE){
+      Cdr(lst);
+    } else { 
+      Set_color_hd(*b,WHITE); 
+    }
+    cur = *lst;
+    while (cur->next != Empty){
       b = cur->next->content;
-      //printf("sz = %lld, color = %d, tag = %d\n",Size(Val_ptr(b+1)),Color(Val_ptr(b+1)),Tag(Val_ptr(b+1)));
-      if (Color(Val_ptr(b+1)) == GRAY){  
-	cons_fl(b,&fl_big_objects); // ajoute b à la freelist 
-	cur->next = cur->next->next; // retire b de la liste de gros objets alloués
-	assert(cur);
+      if (Color_hd(*b) == WHITE){
+        RemoveCadr(&cur);
+      } else {
+        Set_color_hd(*b,WHITE);
+        cur = cur->next;
       }
     }
   }
 }
 
+void sweep (){
+  Debug( printf ("[debut : %d objets alloués\n",length(big_objects)) );
+  sweep_free(&big_objects);
+  Debug( printf ("fin : %d objets alloués]\n",length(big_objects)) );
+}
+
 mlvalue *mark_and_sweep_alloc(size_t sz){
-  static size_t sz_last_gcX3 = 0; // 3 fois la quantité de mémoire utilisé lors du dernier gc
+  static size_t sz_last = 0; // 3 fois la quantité de mémoire utilisé lors du dernier gc
   static size_t cur_sz = 0;
-
-  if (2 * cur_sz > sz_last_gcX3){  // si augmentation de 50 %
-    /* effectivement la stratégie demdandée augmente 
-       les performances en pratique */
-    sz_last_gcX3 = cur_sz * 3;
+  if (2 * cur_sz > sz_last * 3){  // si augmentation de 50 %
+  // effectivement la stratégie demdandée augmente 
+     //  les performances en pratique 
+    sz_last = cur_sz;
         
-    Debug( printf ("debut : %d objets alloués\n",length(big_objects)) );
-        
-    //mark();
-    sweep(&big_objects);
-
-    Debug( printf ("fin : %d objets alloués\n",length(big_objects)) );
+    mark();
+    sweep();
   }
   cur_sz+=sz;
-  return alloc_big_object(sz); //malloc(sz*8);
+  return alloc_big_object(sz); // return malloc (8*sz);
 }
